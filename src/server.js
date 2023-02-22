ddc5ba0718bf87e49d574414694a6ec3a576b5ee
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-02-22 07:24:39 -0300

    creating migrations

--- src/server.js
+++ src/server.js
@@ -1,18 +1,16 @@
-const expressAsyncErrors = require("express-async-errors");
+require("express-async-errors");
 
-const database = require("./database/sqlite");
+const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError")
 const express = require("express");
 
 const routes = require("./routes");
-const { request } = require("express");
+migrationsRun();
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
-
-database();
 
 app.use(( error, request, response, next ) => {
   if(error instanceof AppError){

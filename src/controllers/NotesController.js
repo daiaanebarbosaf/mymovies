1e5dd8eefb1e868772dc4510d98ca2fbf6715eab
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-02-26 08:13:55 -0300

    deleting notes along with tags

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -37,6 +37,13 @@ class NotesController {
     });
   }
 
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex("movies_notes").where({ id }).delete();
+
+    return response.json();
+  }
 }
 
 module.exports = NotesController;

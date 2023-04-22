ce21623957ff8d713a7f6ed62d6f38abb40c7a57
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-04-22 11:42:25 -0300

    Adaptação para usar Middleware

--- src/middlewares/ensureAuthenticated.js
+++ src/middlewares/ensureAuthenticated.js
@@ -9,7 +9,7 @@ function ensureAuthenticated(request, response, next){
     throw new AppError("JWT Token não informado", 401);
   }
 
-  const [, token] = authHeader.aplit(" ");
+  const [, token] = authHeader.split(" ");
 
   try {
     const { sub: user_id } = verify(token, authConfig.jwt.secret);

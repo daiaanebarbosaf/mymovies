d9b4c853ebd45c470435ef85ffae58593858ccc1
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-05-13 10:54:17 -0300

    Update SessionsController.js

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -2,6 +2,7 @@ const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
 const { compare } = require("bcryptjs");
 const authConfig = require("../configs/auth");
+
 const { sign } = require("jsonwebtoken");
 
 class SessionsController {

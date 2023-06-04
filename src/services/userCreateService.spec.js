e4cf1e2b048f7d07debfb1a1f1a1e5fc4e70ddfd
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-06-04 18:14:58 -0300

    Criando grupos de testes com describe

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,18 +1,23 @@
 const UserCreateService = require('./UserCreateService');
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
 
-it("user shold be create", async () => {
+describe("UserCreateService", () => {
+
+  it("user shold be create", async () => {
+
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123"
+    };
   
-  const user = {
-    name: "User Test",
-    email: "user@test.com",
-    password: "123"
-  };
-
-  const userRepositoryInMemory = new UserRepositoryInMemory();
-  const userCreateService = new UserCreateService(userRepositoryInMemory);
-  const userCreated = await userCreateService.execute(user);
-
-  expect(userCreated).toHaveProperty("id");
-
+    const userRepositoryInMemory = new UserRepositoryInMemory();
+    const userCreateService = new UserCreateService(userRepositoryInMemory);
+    const userCreated = await userCreateService.execute(user);
+  
+    expect(userCreated).toHaveProperty("id");
+  
+  });
+  
 });
+

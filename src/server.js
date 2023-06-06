5a3a9298d5c2dbd1c8346a84cd9e5380e29e9f38
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-06-06 07:51:35 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 require("express-async-errors");
-require("dotenv/config");
 
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
@@ -34,6 +33,6 @@ app.use(( error, request, response, next ) => {
   });
 });
 
-const PORT =  process.env.PORT || 3333;
+const PORT = 3333;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));

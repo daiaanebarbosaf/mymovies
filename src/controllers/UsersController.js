bd4e284e561930528aaea044996f17e7583468a1
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-06-06 07:50:13 -0300

    Update UsersController.js

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,18 +1,24 @@
 const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
-
-const sqliteConnection = require("../database/sqlite");
-const UserRepository = require("../repositories/UserRepository");
-const UserCreateService = require("../services/UserCreateService");
+const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
   async create(request, response){
-    const { name, email, password } = request.body;
+    const { name, email, password} = request.body;
 
-    const userRepository = UserRepository();
-    const userCreateService = UserCreateService(userRepository);
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-    await userCreateService.execute({name, email, password});
+    if(checkUserExists){
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await  database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)", 
+      [name, email, hashedPassword]
+    );
 
     return response.status(201).json();
 
